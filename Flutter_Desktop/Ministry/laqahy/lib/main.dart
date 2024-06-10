9a69898496a26e87eca2380d6c03e915cb7af718
Eng-Konan <154022487+Eng-Konan@users.noreply.github.com>
2024-06-10 14:09:25 +0300

    Editing on some code

--- Flutter_Desktop/Ministry/laqahy/lib/main.dart
+++ Flutter_Desktop/Ministry/laqahy/lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
       ],
       supportedLocales: const [Locale("ar", "AE")],
       locale: const Locale("ar", "AL"),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
